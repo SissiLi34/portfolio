2d671e87dafbdf73529214c5c7d59b05fdd38586
SissiLi34 <92378904+SissiLi34@users.noreply.github.com>
2022-01-26 16:14:04 +0100

    first new commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//let view_gallery = document.querySelector(".view-gallery");
+//let button =  document.querySelector("button");
+
+//view_gallery.addEventListener('click', function () {
+    //console.log("loliloll");
+    //$('.box').toggleClass('animated');
+   // $('ul').css({
+       // 'max-width' : '100%'
+       // });
+//});
+
+$(".view-gallery button").on("click", function () {
+    $(".box").toggleClass("animated");
+    // $('ul').css({
+    //   'max-width' : '100%'
+    // });
+  });
+
+
+
+
+
